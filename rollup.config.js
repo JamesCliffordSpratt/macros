cb81ff594f70a549a23d6f04f20b29150373b3e4
Spratt94 <72018651+Spratt94@users.noreply.github.com>
2025-04-16 23:31:30 +0100

    Chore: update config for src folder structure

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'; // <-- Add this
 
 export default {
-  input: 'main.ts',
+  input: 'src/main.ts',
   output: {
     dir: '.',
     format: 'cjs',
