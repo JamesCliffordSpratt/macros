4dd0b2007fd05c3a26984d1e09adf1471f9bf5fd
James Clifford Spratt <jamescliffordspratt@icloud.com>
2026-06-27 15:52:18 +0100

    Add ESLint flat config and toolchain deps

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+// eslint.config.mjs
+import tsparser from "@typescript-eslint/parser";
+import obsidianmd from "eslint-plugin-obsidianmd";
+export default [
+  ...obsidianmd.configs.recommended,
+  { files: ["**/*.ts"],
+    languageOptions: { parser: tsparser, parserOptions: { project: "./tsconfig.json" } } },
+];
